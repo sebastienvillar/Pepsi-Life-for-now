3f8936c9b7f4fc271aae0727b12947654896b02a
Sebastien Villar <sebastien.villar@gmail.com>
2013-07-26 17:40:04 +0200

    Cells in Me

--- iOS/www/js/controllers/meController.js
+++ iOS/www/js/controllers/meController.js
@@ -1,64 +1,173 @@
 var requireArray = [
 	"controllers/controller",
-	"views/tableView"
+	"views/tableView",
+	"helpers/serverRequest",
+	"views/imageCell",
+	"models/post"
 ]
 
-define(requireArray, function(Controller, TableView) {
-	var MeController = function() {
-		Controller.call(this);
+define(requireArray, function(Controller, TableView, ServerRequest, ImageCell, Post) {
+var MeController = function() {
+	Controller.call(this);
 
-		this.$container.attr("id", "meController");
+	this.$container.attr("id", "meController");
 
-		this.tableView = new TableView();
-		this.tableView.$container.attr("id", "tableView");
-		this.tableView.$cellsContainer.attr("id", "cellsContainer");
-		this.tableView.$container.appendTo(this.$container);
+	this.tableView = new TableView();
+	this.tableView.$container.attr("id", "tableView");
+	this.tableView.$cellsContainer.attr("id", "cellsContainer");
+	this.tableView.$container.appendTo(this.$container);
 
-		this.$header = $("<div>", {"id": "header"});
-		this.tableView.setHeader(this.$header);
+	this.$header = $("<div>", {"id": "header"});
+	this.tableView.setHeader(this.$header);
 
-		this.$avatarWrapper = $("<div>", {"id": "avatarWrapper"});
-		this.$avatarWrapper.appendTo(this.$header);
+	this.$avatarWrapper = $("<div>", {"id": "avatarWrapper"});
+	this.$avatarWrapper.appendTo(this.$header);
 
-		this.$avatar = $("<div>", {"id": "avatar"});
-		this.$avatar.appendTo(this.$avatarWrapper);
+	this.$avatar = $("<div>", {"id": "avatar"});
+	this.$avatar.appendTo(this.$avatarWrapper);
 
-		this.$username = $("<p>", {"id": "username"});
-		this.$username.appendTo(this.$header);
-		this.$username.text("PAUL WALKER");
+	this.$username = $("<p>", {"id": "username"});
+	this.$username.appendTo(this.$header);
+	this.$username.text("PAUL WALKER");
 
-		this.$description = $("<p>", {"id": "description"});
-		this.$description.appendTo(this.$header);
-		this.$description.text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse imperdiet libero id neque fringilla, in.");
+	this.$description = $("<p>", {"id": "description"});
+	this.$description.appendTo(this.$header);
+	this.$description.text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse imperdiet libero id neque fringilla, in.");
 
-		this.$postsRect = $("<div>", {"id": "postsRect"});
-		this.$postsRect.appendTo(this.$header);
+	this.$postsRect = $("<div>", {"id": "postsRect"});
+	this.$postsRect.appendTo(this.$header);
 
-		this.$postsCount = $("<span>", {"id": "postsCount"});
-		this.$postsCount.text("54");
-		this.$postsCount.addClass("rectText");
-		this.$postsCount.appendTo(this.$postsRect);
+	this.$postsCount = $("<span>", {"id": "postsCount"});
+	this.$postsCount.text("54");
+	this.$postsCount.addClass("rectText");
+	this.$postsCount.appendTo(this.$postsRect);
 
-		this.$postsText = $("<span>", {"id": "postsText"});
-		this.$postsText.text("POSTS");
-		this.$postsText.addClass("rectText");
-		this.$postsText.appendTo(this.$postsRect);
+	this.$postsText = $("<span>", {"id": "postsText"});
+	this.$postsText.text("POSTS");
+	this.$postsText.addClass("rectText");
+	this.$postsText.appendTo(this.$postsRect);
 
-		this.$likesRect = $("<div>", {"id": "likesRect"});
-		this.$likesRect.appendTo(this.$header);
+	this.$likesRect = $("<div>", {"id": "likesRect"});
+	this.$likesRect.appendTo(this.$header);
 
-		this.$likesCount = $("<span>", {"id": "likesCount"});
-		this.$likesCount.appendTo(this.$likesRect);
-		this.$likesCount.addClass("rectText");
-		this.$likesCount.text("134");
+	this.$likesCount = $("<span>", {"id": "likesCount"});
+	this.$likesCount.appendTo(this.$likesRect);
+	this.$likesCount.addClass("rectText");
+	this.$likesCount.text("134");
 
-		this.$likesText = $("<span>", {"id": "likesText"});
-		this.$likesText.appendTo(this.$likesRect);
-		this.$likesText.addClass("rectText");
-		this.$likesText.text("LIKES");
-		
+	this.$likesText = $("<span>", {"id": "likesText"});
+	this.$likesText.appendTo(this.$likesRect);
+	this.$likesText.addClass("rectText");
+	this.$likesText.text("LIKES");
+
+	this.postsRemaining = true;
+	this.posts = [];
+
+	this.init();
+
+	//Event Handlers
+	this.tableView.on("didScrollToBottom", this._didScrollToBottom.bind(this));
+	this.tableView.on("didSelectRow", this._didSelectRow.bind(this));
+	this.tableView.on("rowIsVisible", this._rowIsVisible.bind(this));
+	
+}
+
+MeController.prototype = new Controller();
+
+MeController.prototype.init = function() {
+	this.pushNewCells();
+}
+
+MeController.prototype.pushNewCells = function() {
+	this.tableView.enterLoadingMode();
+	var request = new ServerRequest();
+	request.method = "GET";
+	request.path = "me/posts";
+	if (this.posts.length != 0)
+		request.queryParameters["last_id"] = this.posts[this.posts.length - 1].id
+
+	request.onSuccess = function(json) {
+		this.tableView.exitLoadingMode();
+		var posts = json.posts;
+		this.postsRemaining = posts.length == 10;
+		for (var i in posts) {
+			var post = Post.postFromJSONObject(posts[i]);
+			this.posts.push(post);
+			var cell = new ImageCell(post);
+
+			cell.on("didClickLike", this._didClickLike.bind(this, cell, post));
+			cell.on("didClickComment", this._didClickComment.bind(this, cell, post));
+
+			this.tableView.pushCell(cell);
+		}
+	}.bind(this);
+	request.onError = function(statusCode, message) {
+		this.tableView.exitLoadingMode();
+		alert("Error in MeController get posts request: " + statusCode + ": " + message);
+	}.bind(this);
+	request.execute();
+};
+
+
+////////////////
+// Private
+///////////////
+
+MeController.prototype._didScrollToBottom = function() {
+	if (!this.tableView.loading && this.postsRemaining) {
+		this.pushNewCells();
 	}
+};
 
-	MeController.prototype = new Controller();
-	return MeController;
+MeController.prototype._didSelectRow = function(row) {
+
+};
+
+MeController.prototype._didClickLike = function(cell, post) {
+	if (post.liked)
+		return;
+
+	post.liked = true;
+	var request = new ServerRequest();
+	request.path = "posts/" + post.id + "/likes";
+	request.method = "POST";
+	request.onSuccess = function(json) {
+		cell.setLikesCount(cell.getLikesCount() + 1);
+	}.bind(this);
+	request.onError = function(status, message) {
+		if (statusCode != 403) {
+			post.like = false;
+			alert("Error in Friends post like request: " + statusCode + ": " + message);
+		}
+	}.bind(this);
+	request.execute();
+};
+
+MeController.prototype._didClickComment = function(cell, post) {
+
+};
+
+MeController.prototype._rowIsVisible = function(row) {
+	var post = this.posts[row];
+	var cell = this.tableView.cellForRow(row);
+	if (post.seen)
+		return
+
+	post.seen = true;
+	var request = new ServerRequest();
+	request.path = "posts/" + post.id + "/seens";
+	request.method = "POST";
+	request.onSuccess = function(json) {
+		cell.setSeensCount(cell.getSeensCount() + 1);
+	}.bind(this);
+	request.onError = function(status, message) {
+		if (statusCode != 403) {
+			alert("Error in Friends post seen request: " + statusCode + ": " + message);
+			post.seen = false;
+		}
+	}.bind(this);
+	request.execute();
+};
+
+return MeController;
 });
