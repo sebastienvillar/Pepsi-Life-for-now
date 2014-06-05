ef0afac2e2e97392fe7dcf3543be3176d456c94f
Sébastien Villar <me@sebastienvillar.com>
2014-06-05 22:41:53 +0200

    Update constants.js

--- Pepsi/www/js/helpers/constants.js
+++ Pepsi/www/js/helpers/constants.js
@@ -2,7 +2,7 @@
 define(function() {
 	var Constants = {
 		SERVER_URL: "https://pepsi-reunion.herokuapp.com/",
-		SERVER_API_KEY: "U6U2XoC4auaMm3fJoV"
+		SERVER_API_KEY: "key"
 	};
 	return Constants;
 });
